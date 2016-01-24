3a2a250c5c65216deed31c4fd6d4d78c83216f79
William Thomas <will.thomas@inmerge.com>
2016-01-23 21:06:11 -0700

    added protractor wrapper

--- CoreUITests/NavTests.cs
+++ CoreUITests/NavTests.cs
@@ -3,6 +3,7 @@ using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using Protractor;
 using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace CoreUITests
@@ -15,7 +16,7 @@ namespace CoreUITests
         [TestMethod]
         public void DashboardTitle()
         {
-            using (IWebDriver wdriver = new ChromeDriver(@"dependencies"))
+            using (IWebDriver wdriver = new NgWebDriver(new ChromeDriver(@"dependencies")))
             {
                 wdriver.Navigate().GoToUrl(baseUrl);
                 wdriver.Manage().Window.Maximize();
@@ -27,7 +28,7 @@ namespace CoreUITests
         [TestMethod]
         public void PeopleTitle()
         {
-            using (IWebDriver wdriver = new ChromeDriver(@"dependencies"))
+            using (IWebDriver wdriver = new NgWebDriver(new ChromeDriver(@"dependencies")))
             {
                 wdriver.Navigate().GoToUrl(baseUrl + "people");
                 wdriver.Manage().Window.Maximize();
