81c1c718eeb97e87d0f44e2a661d32cabc45baf0
William Thomas <will.thomas@inmerge.com>
2016-01-23 22:34:33 -0700

    converted nav tests to use nunit

--- CoreUITests/NavTests.cs
+++ CoreUITests/NavTests.cs
@@ -1,40 +1,59 @@
 ﻿using System;
-using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.PhantomJS;
 using Protractor;
 using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace CoreUITests
 {
-    [TestClass]
+    [TestFixture]
     public class CoreUI
     {
-       // public static string baseUrl = "http://localhost:10101/";
-        public static string baseUrl = "http://coreui.azurewebsites.net/";
+        // public static string baseUrl = "http://localhost:10101/";
+        private const string BaseUrl = "http://coreui.azurewebsites.net/";
+        private IWebDriver _driver;
 
-        [TestMethod]
+        [SetUp]
+        public void SetUp()
+        {
+            // Using NuGet Package 'PhantomJS'
+            //_driver = new NgWebDriver(new PhantomJSDriver(@"dependencies"));
+
+            // Using NuGet Package 'WebDriver.ChromeDriver.win32'
+            _driver = new NgWebDriver(new ChromeDriver(@"dependencies"));
+
+            // Using NuGet Package 'WebDriver.IEDriverServer.win32'
+            //var options = new InternetExplorerOptions() { IntroduceInstabilityByIgnoringProtectedModeSettings = true };
+            //driver = new InternetExplorerDriver(options);
+
+            _driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(5));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+
+        [Test(Description = "Dashboard should have title of ui.core: dashboard")]
         public void DashboardTitle()
         {
-            using (IWebDriver wdriver = new NgWebDriver(new ChromeDriver(@"dependencies")))
+            using (_driver)
             {
-                wdriver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(5));
-                wdriver.Navigate().GoToUrl(baseUrl);
-                Assert.AreEqual(wdriver.Title, "ui.core: dashboard");
-                wdriver.Quit();
+                _driver.Navigate().GoToUrl(BaseUrl);
+                Assert.AreEqual(_driver.Title, "ui.core: dashboard");
             }
         }
 
-        [TestMethod]
+        [Test(Description = "People should have title of ui.core: people")]
         public void PeopleTitle()
         {
-            using (IWebDriver wdriver = new NgWebDriver(new ChromeDriver(@"dependencies")))
+            using (_driver)
             {
-                wdriver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(5));
-                wdriver.Navigate().GoToUrl(baseUrl + "people");
-                Assert.AreEqual(wdriver.Title, "ui.core: people");
-                wdriver.Quit();
+                _driver.Navigate().GoToUrl(BaseUrl + "people");
+                Assert.AreEqual(_driver.Title, "ui.core: people");
             }
         }
     }
