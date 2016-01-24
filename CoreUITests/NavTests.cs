66ea5d0d838aec14ef292e4f83e6463570922ede
William Thomas <will.thomas@inmerge.com>
2016-01-23 18:19:05 -0700

    initial commit

---
+++ CoreUITests/NavTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace CoreUITests
+{
+    [TestClass]
+    public class CoreUI
+    {
+        public static string baseUrl = "http://localhost:10101/";
+
+        [TestMethod]
+        public void DashboardTitle()
+        {
+            using (IWebDriver wdriver = new ChromeDriver(@"dependencies"))
+            {
+                wdriver.Navigate().GoToUrl(baseUrl);
+                wdriver.Manage().Window.Maximize();
+                Assert.AreEqual(wdriver.Title, "ui.core: dashboard");
+                wdriver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void PeopleTitle()
+        {
+            using (IWebDriver wdriver = new ChromeDriver(@"dependencies"))
+            {
+                wdriver.Navigate().GoToUrl(baseUrl + "people");
+                wdriver.Manage().Window.Maximize();
+                Assert.AreEqual(wdriver.Title, "ui.core: people");
+                wdriver.Quit();
+            }
+        }
+    }
+}
---
+++ SeleniumEnd2End/CoreTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumEnd2End
+{
+    class CoreTests
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://coreui.azurewebsites.net/";
+
+            var pageTitle = driver.Title;
+            Assert.AreEqual("Google", selenium.GetTitle());
+        }
+
+        
+    }
+}
